24d5a92268d4acc33ada315e92c4db462eb64b48
amitchen2 <amitchen22@gmail.com>
2022-07-31 08:14:22 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
+import ReactDOM = require('react-dom');
 import './style.css';
 
+function Hello2() {
+  return React.createElement('div', null, 'Hello React 2 !');
+}
+
+function Button(props) {
+  const handleClick = ()=> {props.onClickFunction(props.increment)};
+  return <button onClick={handleClick}>+{props.increment}</button>;
+}
+
+function Display(props) {
+  return <div>{props.message}</div>;
+}
+
 export default function App() {
+  const [counter, setCounter] = React.useState(10);
+  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <React.Fragment>
+      <Hello2 />
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <Button onClickFunction={incrementCounter} increment={5}/>
+      <Button onClickFunction={incrementCounter} increment={10}/>
+      <Button onClickFunction={incrementCounter} increment={100}/>
+
+      <Display message={counter} />
+    </React.Fragment>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
+import ReactDOM=require('react-dom');
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-
+// import Hello from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+
+// ReactDOM.render(
+//   <Hello />,
+//   document.getElementById('root'),
+// );
 root.render(
   <StrictMode>
-    <App />
+  <App/>
   </StrictMode>
 );
